ac3d40e7c6724275c5000dd59f5458db496b788b
Vladimir Jara <vlado.jara@outlook.com>
2013-07-11 14:07:14 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,8 +3,18 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2');
+  response.send(fs.readFile('index.html', function (err, data) {
+      if (err) throw err;
+      console.log(data);
+      });
+	       );
 });
+/*
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+   console.log(data);
+});
+*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
