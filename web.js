e4947ecb2819af32b3a2ce56d5964f68eb7c4af8
Vladimir Jara <vlado.jara@outlook.com>
2013-07-11 14:58:35 +0000

    Added

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var txt=fs.readFileSync('index.html');
 var txt2=buf.toString('utf8');
 
 app.get('/', function(request, response) {
-  response.send(txt);
+  response.send(txt2);
 });
 
 var port = process.env.PORT || 5000;
