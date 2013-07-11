5da618437548edc37967d075c15c8c64e50fef33
Vladimir Jara <vlado.jara@outlook.com>
2013-07-11 15:09:14 +0000

    Added

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs=require('fs');
-var buf=require('buf');
+//var buf=require('buf');
 var txt=fs.readFileSync('index.html',encoding='utf8');
 //var txt1=fs.readFileSync('index.html');
 //var txt2=txt1.toString(encoding='utf8');
