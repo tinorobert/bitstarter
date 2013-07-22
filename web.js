66af8453c4a33438e06538e8e95fa474645a4fd1
Ubuntu <ubuntu@ip-172-31-28-138.us-west-2.compute.internal>
2013-07-22 16:47:28 +0000

    web.js 8080

--- web.js
+++ web.js
@@ -1,19 +1,16 @@
 var express = require('express');
+var fs=require('fs');
+var htmlfile="index.html";
 
 var app = express.createServer(express.logger());
 
-var fs=require('fs');
-//var buf=require('buf');
-var txt=fs.readFileSync('index.html',encoding='utf8');
-var txt1=fs.readFileSync('index.html');
-var txt2=txt1.toString(encoding='utf8');
 
-app.get('/', function(request, response) {
-//  response.send(txt);
-  response.send(txt2);
+app.get('/', function(request, response) {  
+  var html = fs.readFileSync(htmlfile).toString();	
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
